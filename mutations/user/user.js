08485212ca41227f8cca731485a0d8024b01553e
Martin Kapal <flamecze@gmail.com>
2018-03-17 00:08:27 +0100

    delete users

--- mutations/user/user.js
+++ mutations/user/user.js
@@ -1,42 +1,60 @@
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLBoolean,
 } = require('graphql');
-const db = require("../../models/index");
-const bcrypt = require("bcrypt");
+const db = require('../../models/index');
+const bcrypt = require('bcrypt');
 const adminGuard = require('../../guards/admin-guard');
 
 module.exports = new GraphQLObjectType({
-    name: 'UserMutation',
-    description: 'A root for all user related mutations',
-    fields: {
-        create: {
-            args: {
-                username: {
-                    type:  GraphQLNonNull(GraphQLString),
-                },
-                password: {
-                    type:  GraphQLNonNull(GraphQLString),
-                }
-            },
-            type: require('../../objects/user'),
-            description: 'Creates a new user.',
-            async resolve(root, { username, password }) {
-                return await db['user'].create({
-                    username: username.toLowerCase(),
-                    password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
-                });
-            }
+  name: 'UserMutation',
+  description: 'A root for all user related mutations',
+  fields: {
+    new: {
+      args: {
+        username: {
+          type: GraphQLNonNull(GraphQLString),
         },
+        password: {
+          type: GraphQLNonNull(GraphQLString),
+        }
+      },
+      type: require('../../objects/user'),
+      description: 'Creates a new user.',
+      async resolve(root, { username, password }) {
+        return await db['user'].create({
+          username: username.toLowerCase(),
+          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
+        });
+      }
+    },
+    delete: {
+      args: {
+        id: {
+          type: GraphQLNonNull(GraphQLInt),
+        }
+      },
+      type: GraphQLBoolean,
+      async resolve(root, { id }) {
+        const user = await db['user'].findById(id);
+        if (!user) {
+          return false;
+        }
 
-        logToken: {
-            type: GraphQLString,
-            description: 'console logs current user.',
-            async resolve(root, { username, password }, req) {
-                console.log(await adminGuard(req));
-                return 'test';
-            }
-        },
-    }
+        await user.destroy();
+        return true;
+      }
+    },
+    logToken: {
+      type: GraphQLString,
+      description: 'console logs current user.',
+      async resolve(root, { username, password }, req) {
+        console.log(await adminGuard(req));
+        return 'test';
+      }
+    },
+  }
 });
